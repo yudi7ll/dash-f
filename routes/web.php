9664fc126ae5d0fb6946ed0cae148aa224d02577
Yudi <madewahyudi22@gmail.com>
2020-07-05 14:33:50 +0800

    auth scaffolding

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'PostController@index');
+Route::get('/', 'PostController@index')->name('home');
 
-Route::get('/login', function () {
-    return view('auth.login');
-});
+Auth::routes();
+
+// Route::get('/home', 'HomeController@index')->name('home');
